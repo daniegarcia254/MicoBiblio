601bcdd199cb43a2cbbda5d89d409f4cf48fabb4
daniegarcia254 <daniegarcia254@gmail.com>
2018-01-29 15:15:12 +0100

    Change config method to form connection URL to mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,39 @@
+env = process.env.NODE_ENV || 'development';
+
 module.exports = {
     //MongoDB configuration
     development: {
-	port: 10008,
-        db: 'mongodb://127.0.0.1/micobiblio',
+	    port: 10008,
+        //db: 'mongodb://127.0.0.1/micobiblio',
+        db: {
+            base: 'mongodb://',
+            user: 'dgarcia',
+            password: '',
+            host: 'danigarcia-dev.com',
+            port: '10007',
+            name: 'micobiblio'
+        },
         app: {
             name: 'micobiblio'
         }
     },
     production: {
-        db: 'mongodb://<username>:<password>@ds157325.mlab.com:57325/micobiblio-prod',
+        db: {
+            base: 'mongodb://',
+            user: '<username>',
+            password: '<password>',
+            host: 'danigarcia-dev.com',
+            port: '10007',
+            name: 'micobiblio'
+        },
         app: {
             name: 'micobiblio'
         }
+    },
+    getDatabaseURL: function() {
+        var dbData = this[env].db;
+        var dbUser = dbData.user + (dbData.password ? (':' + dbData.password) : '');
+        var dbHost = dbData.host + (dbData.port ? (':' + dbData.port) : '');
+        return dbData.base + dbUser + '@' + dbHost + '/' + dbData.name;
     }
 };
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,10 +1,10 @@
 var env = process.env.NODE_ENV || 'development',
-    config = require('./config')[env],
+    config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
     mongoose.Promise = global.Promise;
-    var db = mongoose.connect(config.db, { useMongoClient: true });
+    var db = mongoose.connect(config.getDatabaseURL(), { useMongoClient: true });
     mongoose.connection.on('error', function (err) {
         console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?', err);
     }).on('open', function () {
